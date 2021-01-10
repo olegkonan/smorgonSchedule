a151c822501cf3586ecbf9d2cbe218ecc1d75317
Oleg Konan <olegkonanff@gmail.com>
2021-01-10 12:47:11 +0300

    redesign to js

--- includes/getstop.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'includes/connect.php';
-    $sql = "SELECT * FROM allstops ORDER BY stop_name ASC";
-    $result = mysqli_query($link, $sql);
-    while ($row = mysqli_fetch_array($result)) {
-        echo "<option value='" . $row['stop'] . "'>" . $row['stop_name'] . "</option>";
-    }
-    mysqli_close($link);
-?>
